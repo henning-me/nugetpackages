920079e51dab876001ba2611149d5e7fd2625966
Henning Bredenkötter <henning@henning.me>
2014-10-20 10:42:07 +0200

    Update to 2.10.0.4

--- ncrunch2.gridnodeserver/tools/chocolateyInstall.ps1
+++ ncrunch2.gridnodeserver/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
-﻿$packageName = 'ncrunch2.gridnodeserver' 
-$url = 'http://downloads.ncrunch.net/NCrunch_GridNodeServer_2.7.0.5.msi' 
+﻿$packageName = 'ncrunch2.gridnodeserver'
+$url = 'http://downloads.ncrunch.net/NCrunch_GridNodeServer_2.10.0.4.msi'
 
-$installerType = 'msi' 
-$silentArgs = '/quiet' 
-$validExitCodes = @(0) 
+$installerType = 'msi'
+$silentArgs = '/quiet'
+$validExitCodes = @(0)
 Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes
--- ncrunch2.vs2012/tools/chocolateyInstall.ps1
+++ ncrunch2.vs2012/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
-﻿$packageName = 'ncrunch2.vs2012' 
-$url = 'http://downloads.ncrunch.net/NCrunch_VS2012_2.7.0.5.msi' 
+﻿$packageName = 'ncrunch2.vs2012'
+$url = 'http://downloads.ncrunch.net/NCrunch_VS2012_2.10.0.4.msi'
 
-$installerType = 'msi' 
-$silentArgs = '/quiet' 
-$validExitCodes = @(0) 
+$installerType = 'msi'
+$silentArgs = '/quiet'
+$validExitCodes = @(0)
 Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -validExitCodes $validExitCodes
