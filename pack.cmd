e347cd61a9a6400749522a31d332425403d315f6
Henning Bredenkötter <henning@henning.me>
2015-02-02 10:13:35 +0100

    CHG Fixed Pack Script with Comments

--- pack.cmd
+++ pack.cmd
@@ -1,5 +1,8 @@
 @ECHO PACKING...
+del *.nupkg
 cpack ncrunch-vs2015\ncrunch-vs2015.nuspec
 cpack ncrunch2.gridnodeserver\ncrunch2.gridnodeserver.nuspec
 cpack ncrunch2.vs2012\ncrunch2.vs2012.nuspec
 cpack ncrunch2.vs2013\ncrunch2.vs2013.nuspec
+@ECHO Now sending?
+@ECHO choco push *.nupkg
