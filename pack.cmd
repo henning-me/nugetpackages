fcac492ee907a3bf73de661b61faf9a0e89662a0
Henning Bredenkötter <henning@henning.me>
2015-04-08 09:58:48 +0200

    Update pack script

--- pack.cmd
+++ pack.cmd
@@ -1,8 +1,20 @@
 @ECHO PACKING...
 del *.nupkg
-cpack ncrunch-vs2015\ncrunch-vs2015.nuspec
-cpack ncrunch2.gridnodeserver\ncrunch2.gridnodeserver.nuspec
-cpack ncrunch2.vs2012\ncrunch2.vs2012.nuspec
-cpack ncrunch2.vs2013\ncrunch2.vs2013.nuspec
+cd ncrunch-vs2015
+cpack
+mv *.nupkg ..
+cd ..
+cd ncrunch2.gridnodeserver
+cpack
+mv *.nupkg ..
+cd ..
+cd ncrunch2.vs2012
+cpack
+mv *.nupkg ..
+cd ..
+cd ncrunch2.vs2013
+cpack
+mv *.nupkg ..
+cd ..
 @ECHO Now sending?
 @ECHO choco push *.nupkg
