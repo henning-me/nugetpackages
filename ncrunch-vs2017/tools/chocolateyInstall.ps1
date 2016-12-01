ed604fd2bcc76bdd2bb28c51e3e60e063fe90be5
Henning Bredenkoetter <henning@bredenkoetter.de>
2016-12-01 20:05:41 +0100

    Neues Paket für vs2017 erzeugt

---
+++ ncrunch-vs2017/tools/chocolateyInstall.ps1
@@ -0,0 +1,19 @@
+﻿
+
+$packageName = 'ncrunch-vs2017'
+$url = 'http://downloads.ncrunch.net/NCrunch_VS2017_2.27.0.3.msi'
+$checksum  = '9cb53d1c7cb19b85a5b45ad915134a9b8a861868d6fae71be4f5398245ab9dd7'
+
+$ErrorActionPreference = 'Stop'
+
+$packageArgs = @{
+  packageName            = $packageName
+  fileType               = 'MSI'
+  url                    = $url
+  checksum               = $checksum
+  checksumType           = 'sha256'
+  checksumType64         = 'sha256'
+  validExitCodes         = @(0)  
+  silentArgs             = '/quiet'
+}
+Install-ChocolateyPackage @packageArgs
